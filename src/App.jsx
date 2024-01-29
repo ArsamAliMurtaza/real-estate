4f7917613134c1312e043efc6cf801cb4d24c569
ArsamAliMurtaza <2017me118@student.uet.edu.pk>
2024-01-29 22:19:38 +0500

    added the routes and the browser router with their pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Routes>
+    <route path="/" element={<Home />} />
+    <route path="/sign-in" element={<Signin />} />
+    <route path="/sign-up" element={<Signup />} />
+    <route path="/profile" element={<Profile />} />
+    <route path="/about" element={<About />} />
+
+    </Routes>
+    </BrowserRouter>
   )
 }
